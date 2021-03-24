2aeff453e1514dbd44adb1ef0549bab93989e799
hasan-ciftci <hasan.ciftci@ankabeta.com>
2021-03-24 11:33:43 +0300

    Root app seperated from main file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ocr/view/login/view/login_view.dart';
-
-import 'core/init/navigation/navigation_routes.dart';
-import 'core/init/navigation/navigation_service.dart';
+import 'package:flutter_ocr/root_app.dart';
 
 Future<void> main() async {
   runApp(FlutterOcr());
-}
-
-class FlutterOcr extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginView(),
-      navigatorKey: NavigationService.instance.navigatorKey,
-      onGenerateRoute: NavigationRoutes.instance.generateRoute,
-    );
-  }
 }
---
+++ lib/root_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ocr/view/login/view/login_view.dart';
+
+import 'core/init/navigation/navigation_routes.dart';
+import 'core/init/navigation/navigation_service.dart';
+
+class FlutterOcr extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginView(),
+      navigatorKey: NavigationService.instance.navigatorKey,
+      onGenerateRoute: NavigationRoutes.instance.generateRoute,
+    );
+  }
+}
