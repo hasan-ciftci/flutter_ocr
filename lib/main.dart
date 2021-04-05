8c52dd3358e1d20158b128d13f4e0de7c96e74c0
hasan-ciftci <hasan.ciftci@ankabeta.com>
2021-04-05 16:08:31 +0300

    Preferences manager initalization in main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ocr/core/init/notifier/provider_service.dart';
+import 'package:flutter_ocr/core/init/preferences/preferences_manager.dart';
 import 'package:flutter_ocr/root_app.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PreferencesManager.preferencesInit();
   runApp(
     MultiProvider(
       providers: [
