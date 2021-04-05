3f6f5a827fe072b2484920cbb6f4d4df84214d0c
hasan-ciftci <hasan.ciftci@ankabeta.com>
2021-04-05 14:38:55 +0300

    Dio service token added as baseoption.

--- lib/core/init/network/network_manager.dart
+++ lib/core/init/network/network_manager.dart
@@ -7,29 +7,24 @@ import 'package:flutter_ocr/core/constants/enums.dart';
 import 'package:flutter_ocr/core/init/preferences/preferences_manager.dart';
 
 class NetworkManager {
-  static NetworkManager _instance;
+  static final NetworkManager _instance = NetworkManager._internal();
 
-  static NetworkManager get instance {
-    _instance ??= NetworkManager._init();
-    return _instance;
-  }
-
+  static NetworkManager get instance => _instance;
   Dio _dio;
 
-  NetworkManager._init() {
-    _dio = Dio();
+  NetworkManager._internal() {
+    BaseOptions baseOptions = BaseOptions(headers: {
+      "Authorization": "Bearer " +
+          PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN)
+    });
+    _dio = Dio(baseOptions);
   }
 
   Future dioPost<T extends BaseModel>(
       {@required String baseURL,
       @required String endPoint,
       @required T model}) async {
-    final response = await _dio.post(baseURL + endPoint,
-        data: model.toJson(),
-        options: Options(headers: {
-          "Authorization": "Bearer " +
-              PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN)
-        }));
+    final response = await _dio.post(baseURL + endPoint, data: model.toJson());
     switch (response.statusCode) {
       case HttpStatus.ok:
         final responseBody = response.data;
@@ -42,14 +37,10 @@ class NetworkManager {
       {@required String baseURL,
       @required String endPoint,
       @required T model}) async {
-    var myToken =
-        PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN);
-    final response = await _dio.get(baseURL + endPoint,
-        queryParameters: model.toJson(),
-        options: Options(headers: {
-          "Authorization": "Bearer " +
-              PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN)
-        }));
+    final response = await _dio.get(
+      baseURL + endPoint,
+      queryParameters: model.toJson(),
+    );
     switch (response.statusCode) {
       case HttpStatus.ok:
         final responseBody = response.data;
@@ -62,12 +53,10 @@ class NetworkManager {
       {@required String baseURL,
       @required String endPoint,
       @required T file}) async {
-    final response = await _dio.post(baseURL + endPoint,
-        data: file,
-        options: Options(headers: {
-          "Authorization": "Bearer " +
-              PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN)
-        }));
+    final response = await _dio.post(
+      baseURL + endPoint,
+      data: file,
+    );
     switch (response.statusCode) {
       case HttpStatus.ok:
         final responseBody = response.data;
