1b0f6176bb726cd83d551f41ce3e630619a38303
hasan-ciftci <hasan.ciftci@ankabeta.com>
2021-04-05 13:40:47 +0300

    Records view non ui related functions moved to viewmodel. Unnecessary future builder deleted.

--- lib/view/records/view/records_view.dart
+++ lib/view/records/view/records_view.dart
@@ -1,15 +1,9 @@
-import 'dart:convert';
-
 import 'package:connectivity/connectivity.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_ocr/core/components/custom_appbar.dart';
 import 'package:flutter_ocr/core/components/record_card.dart';
-import 'package:flutter_ocr/core/constants/api_constants.dart';
 import 'package:flutter_ocr/core/constants/color_constants.dart';
-import 'package:flutter_ocr/core/init/database/database_service.dart';
-import 'package:flutter_ocr/core/init/network/network_manager.dart';
 import 'package:flutter_ocr/core/init/notifier/provider_service.dart';
 import 'package:flutter_ocr/view/home/model/record_model.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -30,82 +24,6 @@ class _RecordsViewState extends State<RecordsView> {
     super.initState();
     recordsViewModel = RecordsViewModel();
     recordsViewModel.init();
-    getFirstDataOnline();
-  }
-
-  getFirstDataOnline() {
-    ConnectivityResult result =
-        context.read<ConnectionNotifier>().connectivityResult;
-    if (result == ConnectivityResult.none) {
-    } else {
-      recordsViewModel.getMoreData();
-      recordsViewModel.scrollController.addListener(() {
-        if (recordsViewModel.scrollController.position.pixels ==
-            recordsViewModel.scrollController.position.maxScrollExtent) {
-          recordsViewModel.getMoreData();
-        }
-      });
-    }
-  }
-
-  transformOfflineRecordsToFormDataList(
-      List<RecordModel> plates, Map<dynamic, dynamic> recordAndPlateMap) {
-    List<FormData> bulkRecordFormData = plates.map((e) {
-      recordAndPlateMap.addAll({"plate-${e.timestamp}.jpg": "${e.id}"});
-      return FormData.fromMap(
-        {
-          "Image": MultipartFile.fromBytes(base64Decode(e.base64Image),
-              filename: "plate-${e.timestamp}.jpg"),
-          "LicensePlate": e.plate,
-          "Username": e.username,
-        },
-      );
-    }).toList();
-    return bulkRecordFormData;
-  }
-
-  deleteTransferredOfflineRecords(List<FormData> bulkRecordFormData,
-      Map<dynamic, dynamic> recordAndPlateMap) {
-    bulkRecordFormData.forEach((element) async {
-      try {
-        NetworkManager.instance.dioPostForm(
-            baseURL: ApiConstants.OCR_ENGINE_BASE_URL,
-            endPoint: ApiConstants.BULK_SAVE_ENDPOINT,
-            file: element);
-
-        int savedRecordId =
-            int.parse(recordAndPlateMap[element.files.first.value.filename]);
-        await DatabaseService.instance.removeItem(savedRecordId);
-      } catch (e) {
-        print(e);
-
-        ///Skip deleting local item due to an error.
-      }
-    });
-  }
-
-  Future<bool> checkIfOfflineRecordsExists() async {
-    ConnectivityResult result =
-        context.read<ConnectionNotifier>().connectivityResult;
-    if (result == ConnectivityResult.none) {
-      return false;
-    } else {
-      var plates = await recordsViewModel.getPlates();
-      if (plates != null) {
-        try {
-          Map<dynamic, dynamic> recordAndPlateMap = Map();
-          List<FormData> bulkRecordFormData =
-              transformOfflineRecordsToFormDataList(plates, recordAndPlateMap);
-
-          deleteTransferredOfflineRecords(
-              bulkRecordFormData, recordAndPlateMap);
-        } catch (e) {
-          print(e);
-        }
-        return true;
-      }
-      return false;
-    }
   }
 
   @override
@@ -115,19 +33,10 @@ class _RecordsViewState extends State<RecordsView> {
       backgroundColor: ColorConstants.ISPARK_WHITE,
       appBar: buildAppBar(),
       body: FutureBuilder(
-        future: checkIfOfflineRecordsExists(),
+        future: recordsViewModel.checkIfOfflineRecordsExists(),
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return FutureBuilder(
-              future: buildListView(),
-              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return snapshot.data;
-                } else {
-                  return _buildProgressIndicator();
-                }
-              },
-            );
+            return buildListView();
           } else {
             return Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -143,13 +52,28 @@ class _RecordsViewState extends State<RecordsView> {
     );
   }
 
-  Future<Widget> buildListView() async {
+  buildListView() {
     ConnectivityResult result =
         context.read<ConnectionNotifier>().connectivityResult;
     if (result != ConnectivityResult.none) {
-      return Observer(
-        builder: (BuildContext context) {
-          return buildOnlineListView();
+      return FutureBuilder(
+        future: recordsViewModel.getFirstDataOnline(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Observer(
+              builder: (BuildContext context) {
+                return buildOnlineListView();
+              },
+            );
+          } else {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Verileriniz yükleniyor"),
+                Center(child: _buildMockProgressIndicator()),
+              ],
+            );
+          }
         },
       );
     } else {
--- lib/view/records/viewmodel/records_view_model.dart
+++ lib/view/records/viewmodel/records_view_model.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+
+import 'package:connectivity/connectivity.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ocr/core/constants/api_constants.dart';
 import 'package:flutter_ocr/core/constants/navigation_root_name_constants.dart';
 import 'package:flutter_ocr/core/init/database/database_service.dart';
 import 'package:flutter_ocr/core/init/navigation/navigation_service.dart';
+import 'package:flutter_ocr/core/init/network/network_manager.dart';
 import 'package:flutter_ocr/core/init/notifier/provider_service.dart';
 import 'package:flutter_ocr/view/home/model/record_model.dart';
 import 'package:mobx/mobx.dart';
@@ -71,6 +77,81 @@ abstract class _RecordsViewModelBase with Store {
   Future<List<RecordModel>> getPlates() async {
     var recordList = await recordDataBaseProvider.getRecordList("username");
     return recordList;
+  }
+
+  Future<void> getFirstDataOnline() async {
+    ConnectivityResult result =
+        Provider.of<ConnectionNotifier>(myContext, listen: false)
+            .connectivityResult;
+    if (result != ConnectivityResult.none) {
+      await getMoreData();
+      scrollController.addListener(() async {
+        if (scrollController.position.pixels ==
+            scrollController.position.maxScrollExtent) {
+          await getMoreData();
+        }
+      });
+    }
+  }
+
+  transformOfflineRecordsToFormDataList(
+      List<RecordModel> plates, Map<dynamic, dynamic> recordAndPlateMap) {
+    List<FormData> bulkRecordFormData = plates.map((e) {
+      recordAndPlateMap.addAll({"plate-${e.timestamp}.jpg": "${e.id}"});
+      return FormData.fromMap(
+        {
+          "Image": MultipartFile.fromBytes(base64Decode(e.base64Image),
+              filename: "plate-${e.timestamp}.jpg"),
+          "LicensePlate": e.plate,
+          "Username": e.username,
+        },
+      );
+    }).toList();
+    return bulkRecordFormData;
+  }
+
+  deleteTransferredOfflineRecords(List<FormData> bulkRecordFormData,
+      Map<dynamic, dynamic> recordAndPlateMap) {
+    bulkRecordFormData.forEach((element) async {
+      try {
+        NetworkManager.instance.dioPostForm(
+            baseURL: ApiConstants.OCR_ENGINE_BASE_URL,
+            endPoint: ApiConstants.BULK_SAVE_ENDPOINT,
+            file: element);
+
+        int savedRecordId =
+            int.parse(recordAndPlateMap[element.files.first.value.filename]);
+        await DatabaseService.instance.removeItem(savedRecordId);
+      } catch (e) {
+        print(e);
+
+        ///Skip deleting local item due to an error.
+      }
+    });
+  }
+
+  Future<bool> checkIfOfflineRecordsExists() async {
+    ConnectivityResult result =
+        myContext.read<ConnectionNotifier>().connectivityResult;
+    if (result == ConnectivityResult.none) {
+      return false;
+    } else {
+      var plates = await getPlates();
+      if (plates != null) {
+        try {
+          Map<dynamic, dynamic> recordAndPlateMap = Map();
+          List<FormData> bulkRecordFormData =
+              transformOfflineRecordsToFormDataList(plates, recordAndPlateMap);
+
+          deleteTransferredOfflineRecords(
+              bulkRecordFormData, recordAndPlateMap);
+        } catch (e) {
+          print(e);
+        }
+        return true;
+      }
+      return false;
+    }
   }
 
   navigateToSingleRecordViewPage(int recordId) {
