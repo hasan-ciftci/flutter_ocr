4288f492770381ac5bb5f919997191f03eafd855
hasan-ciftci <hasan.ciftci@ankabeta.com>
2021-04-06 13:27:34 +0300

    Token expire check improvements.

--- lib/core/init/network/network_manager.dart
+++ lib/core/init/network/network_manager.dart
@@ -22,18 +22,24 @@ class NetworkManager {
       {@required String baseURL,
       @required String endPoint,
       @required T model}) async {
-    final response = await _dio.post(baseURL + endPoint,
-        data: model.toJson(),
-        options: Options(headers: {
-          "Authorization": "Bearer " +
-              PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN)
-        }));
-    checkAuthenticationEnded(response);
-    switch (response.statusCode) {
-      case HttpStatus.ok:
-        final responseBody = response.data;
-        return responseBody;
-      default:
+    try {
+      final response = await _dio.post(baseURL + endPoint,
+          data: model.toJson(),
+          options: Options(headers: {
+            "Authorization": "Bearer " +
+                PreferencesManager.instance
+                    .getStringValue(PreferencesKeys.TOKEN)
+          }));
+      switch (response.statusCode) {
+        case HttpStatus.ok:
+          final responseBody = response.data;
+          return responseBody;
+        default:
+      }
+    } catch (e) {
+      if (e is DioError) {
+        checkAuthenticationEnded(e);
+      }
     }
   }
 
@@ -41,18 +47,24 @@ class NetworkManager {
       {@required String baseURL,
       @required String endPoint,
       @required T model}) async {
-    final response = await _dio.get(baseURL + endPoint,
-        queryParameters: model.toJson(),
-        options: Options(headers: {
-          "Authorization": "Bearer " +
-              PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN)
-        }));
-    checkAuthenticationEnded(response);
-    switch (response.statusCode) {
-      case HttpStatus.ok:
-        final responseBody = response.data;
-        return responseBody;
-      default:
+    try {
+      final response = await _dio.get(baseURL + endPoint,
+          queryParameters: model.toJson(),
+          options: Options(headers: {
+            "Authorization": "Bearer " +
+                PreferencesManager.instance
+                    .getStringValue(PreferencesKeys.TOKEN)
+          }));
+      switch (response.statusCode) {
+        case HttpStatus.ok:
+          final responseBody = response.data;
+          return responseBody;
+        default:
+      }
+    } catch (e) {
+      if (e is DioError) {
+        checkAuthenticationEnded(e);
+      }
     }
   }
 
@@ -60,26 +72,33 @@ class NetworkManager {
       {@required String baseURL,
       @required String endPoint,
       @required T file}) async {
-    final response = await _dio.post(baseURL + endPoint,
-        data: file,
-        options: Options(headers: {
-          "Authorization": "Bearer " +
-              PreferencesManager.instance.getStringValue(PreferencesKeys.TOKEN)
-        }));
-    checkAuthenticationEnded(response);
-    switch (response.statusCode) {
-      case HttpStatus.ok:
-        final responseBody = response.data;
-        return responseBody;
-      default:
+    try {
+      final response = await _dio.post(baseURL + endPoint,
+          data: file,
+          options: Options(headers: {
+            "Authorization": "Bearer " +
+                PreferencesManager.instance
+                    .getStringValue(PreferencesKeys.TOKEN)
+          }));
+      switch (response.statusCode) {
+        case HttpStatus.ok:
+          final responseBody = response.data;
+          return responseBody;
+        default:
+      }
+    } catch (e) {
+      if (e is DioError) {
+        checkAuthenticationEnded(e);
+      }
     }
   }
+}
 
-  checkAuthenticationEnded(Response<dynamic> response) {
-    if (response.data.toString().contains("401")) {
-      NavigationService.instance
-          .navigateToPageClear(path: NavigationConstants.LOGIN_VIEW);
-      PreferencesManager.instance.clearAll();
-    }
+checkAuthenticationEnded(DioError e) {
+  if (e.error.toString().contains("401")) {
+    NavigationService.instance
+        .navigateToPageClear(path: NavigationConstants.LOGIN_VIEW);
+
+    PreferencesManager.instance.clearAll();
   }
 }
