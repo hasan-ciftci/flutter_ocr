3d975bf02c61ac65e91b621612a7616a4d44f56b
Semih Küçükmidil <semih.kucukmidil@ankabeta.com>
2021-04-08 10:28:29 +0300

    App ıcon and theme data primary color changed for recent apps screen.

--- lib/root_app.dart
+++ lib/root_app.dart
@@ -4,6 +4,9 @@ import 'package:flutter_ocr/core/init/preferences/preferences_manager.dart';
 import 'package:flutter_ocr/view/home/view/home_view.dart';
 import 'package:flutter_ocr/view/login/view/login_view.dart';
 
+import 'core/constants/color_constants.dart';
+import 'core/constants/color_constants.dart';
+import 'core/constants/color_constants.dart';
 import 'core/init/navigation/navigation_routes.dart';
 import 'core/init/navigation/navigation_service.dart';
 
@@ -19,6 +22,10 @@ class FlutterOcr extends StatelessWidget {
           : LoginView(),
       navigatorKey: NavigationService.instance.navigatorKey,
       onGenerateRoute: NavigationRoutes.instance.generateRoute,
+      theme: ThemeData(
+          primaryColor: ColorConstants.ISPARK_YELLOW,
+          primaryColorLight: ColorConstants.ISPARK_YELLOW,
+          primaryColorDark: ColorConstants.ISPARK_YELLOW),
     );
   }
 }
